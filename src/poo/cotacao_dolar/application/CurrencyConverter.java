3e2646d6d6686c0ea23286e9d728bdfe5cdb1eea
Otávio Neto <netootavio223@outlook.com>
2024-04-08 18:14:52 -0300

    adição dos arquivos

---
+++ src/poo/cotacao_dolar/application/CurrencyConverter.java
@@ -0,0 +1,23 @@
+package poo.cotacao_dolar.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CurrencyConverter {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is the dollar price?");
+        double PRICE = sc.nextDouble();
+
+        System.out.println("How many dollars will be bought?");
+        double DOLLAR = sc.nextDouble();
+
+        double REAL = (DOLLAR * PRICE) * 1.06;
+        System.out.printf("Amount to be paid in reais: %.2f", REAL);
+
+
+        sc.close();
+    }
+}
